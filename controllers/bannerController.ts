2ec7dbec1fbd2cef38db164d536e5b14b432604c
rohit-simbanic <rohit.simbanic2023@gmail.com>
2024-06-18 16:25:01 +0530

    18th june

--- controllers/bannerController.ts
+++ controllers/bannerController.ts
@@ -1,9 +1,8 @@
 import { Request, Response } from "express";
 
 const bannerData = {
-  banner_header: "Looking To Sell?",
-  banner_details_text:
-    "Wonder what your house is really worth? I provide a complimentary detailed home evaluation which will help you determine the value of your home. Even if you are not considering selling your home today, home evaluation is important because it can provide a picture of your overall financial health.",
+  banner_header: "FIND YOUR NEXT REAL ESTATE PROPERTY",
+  banner_details_text: "WE HAVE THE RIGHT PROPERTY FOR YOU",
 };
 
 export const getBanner = (req: Request, res: Response) => {
